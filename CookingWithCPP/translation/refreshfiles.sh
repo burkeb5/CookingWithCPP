b72cf91c3c0311836e87acda445ad9fc1bc0618e
Victor Smith <vsmithiv@gmail.com>
2021-07-05 19:15:30 -0400

    changed things

--- CookingWithCPP/translation/refreshfiles.sh
+++ CookingWithCPP/translation/refreshfiles.sh
@@ -1,2 +1,2 @@
-msgfmt.exe -o en/en.mo ../messages_en.po && \
-msgfmt.exe -o es/es.mo ../messages_es.po
+msgfmt.exe -o en/LC_MESSAGES/hello.mo ../messages_en.po && \
+msgfmt.exe -o es/LC_MESSAGES/hello.mo ../messages_es.po
