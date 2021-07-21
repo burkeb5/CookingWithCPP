fdf7d128a0601870d3e6b77045bb97ba99b256b6
Victor Smith <vsmithiv@gmail.com>
2021-07-21 01:27:29 -0400

    documentation updates to FoodOrderBuilder and IBuilder

--- CookingWithCPP/RestaurantBuilder/FoodOrderBuilder.h
+++ CookingWithCPP/RestaurantBuilder/FoodOrderBuilder.h
@@ -3,20 +3,48 @@
 #include <string>
 #include "IBuilder.h"
 
+/**
+* <summary> Builder class that generates new orders. Creates orders based on a set type which needs be set.</summary>
+* Extends the IBuilder interface.
+* 
+*/
 class RESTAURANTBUILDER_API FoodOrderBuilder : public IBuilder
 {
 public:
-	enum OrderTypes
+	/**
+	* <summary> Represents types of orders available.</summary>
+	*/
+	enum OrderType
 	{
-		PriceFoodOrder,
-		FoodNameOrder
+		PriceFoodOrder,		/**< Order will based on price. */
+		FoodNameOrder		/**< Order will based on name. */
 	};
 
-	void SetType(OrderTypes type);
-	OrderTypes GetType();
+	/**
+	* <summary> Sets the Order Type for this FoodOrderBuilder.</summary>
+	* 
+	*/
+	void SetType(OrderType type);
 
+	/**
+	* <summary> Returns the current Order Type for this FoodOrderBuilder.</summary>
+	*/
+	OrderType GetType();
+
+	/**
+	* <summary> Generates a new order based-on maximizing the provided budget.</summary>
+	* Utilizes the set OrderType on this FoodOrderBuilder.
+	*/
 	void order(int budget);
+
+	/**
+	* <summary> Generates a new minimal food order with the provided foodname.</summary>
+	* Utilizes the set OrderType on this FoodOrderBuilder.
+	*/
 	void order(std::string foodname);
 
+	/**
+	* <summary> Returns the unique ID for this FoodOrderBuilder.</summary>
+	*/
 	int getUniqueID() override;
 };
--- CookingWithCPP/RestaurantBuilder/IBuilder.h
+++ CookingWithCPP/RestaurantBuilder/IBuilder.h
@@ -1,5 +1,15 @@
 #pragma once
 
+/**
+* <summary> Class that all other Builders inherit.</summary>
+*
+* The suggested implementation is for methods to rely on support from methods in [Core](@ref Core).
+*/
 class RESTAURANTBUILDER_API IBuilder {
+	/**
+	* <summary> Returns this unique ID for this IBuilder-inherited object.</summary>
+	*
+	* The suggested implementation is for methods to rely on support from methods in [Core](@ref Core).
+	*/
 	virtual int getUniqueID();
 };
